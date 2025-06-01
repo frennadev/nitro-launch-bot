73a3f1c459cb0b62fa632926de6c949c40d59c69
teepy <56241351+Tee-py@users.noreply.github.com>
2025-06-01 17:20:50 +0100

    added launch success and error message handler

---
+++ src/bot/message.ts
@@ -0,0 +1,72 @@
+import { InlineKeyboard } from "grammy";
+import bot from ".";
+import { CallBackQueries } from "./types";
+import { escape } from "./utils";
+
+export const sendLaunchSuccessNotification = async (
+  chatId: number,
+  tokenAddress: string,
+  tokenName: string,
+  symbol: string,
+  description: string,
+) => {
+  const msg = [
+    `🎉 *Token launched successfully* \n`,
+    `*Name*: ${escape(tokenName)}`,
+    `*Symbol:* $\`${escape(symbol)}\``,
+    `*Description*: _${escape(description || "")}_`,
+    `Click the buttons below to perform other actions ⬇️\n`,
+  ].join("\n");
+  await bot.api.sendMessage(chatId, msg, {
+    parse_mode: "MarkdownV2",
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "👨‍💻 Sell Dev Supply",
+            callback_data: `${CallBackQueries.SELL_DEV}_${tokenAddress}`,
+          },
+          {
+            text: "📈 Sell % supply",
+            callback_data: `${CallBackQueries.SELL_PERCENT}_${tokenAddress}`,
+          },
+        ],
+        [
+          {
+            text: "🧨 Sell All",
+            callback_data: `${CallBackQueries.SELL_ALL}_${tokenAddress}`,
+          },
+        ],
+      ],
+    },
+  });
+};
+
+export const sendLaunchFailureNotification = async (
+  chatId: number,
+  tokenAddress: string,
+  tokenName: string,
+  symbol: string,
+  description: string,
+) => {
+  const msg = [
+    `🎉 *Token launched successfully* \n`,
+    `*Name*: ${escape(tokenName)}`,
+    `*Symbol:* $\`${escape(symbol)}\``,
+    `*Description*: _${escape(description || "")}_`,
+    `Click the buttons below to perform other actions ⬇️\n`,
+  ].join("\n");
+  await bot.api.sendMessage(chatId, msg, {
+    parse_mode: "MarkdownV2",
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "🚀 Launch Token",
+            callback_data: `${CallBackQueries.LAUNCH_TOKEN}_${tokenAddress}`,
+          },
+        ],
+      ],
+    },
+  });
+};
